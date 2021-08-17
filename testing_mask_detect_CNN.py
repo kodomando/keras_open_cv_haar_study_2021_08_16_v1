b2e88ef696bcab3b832d8e31f48acab94426d4fa
kodomando <86344475+kodomando@users.noreply.github.com>
2021-08-17 12:03:03 +0900

    Update testing_mask_detect_CNN.py

--- testing_mask_detect_CNN.py
+++ testing_mask_detect_CNN.py
@@ -1,97 +1,16 @@
 import tensorflow as tf
 import cv2 as cv
-from tensorflow.keras.datasets import cifar10
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten
-from tensorflow.keras.layers import Conv2D, MaxPooling2D
-from tensorflow.keras.utils import to_categorical
 from tensorboard import *
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import os, random, datetime, glob, cv2
-from PIL import Image
 from tqdm import tqdm
 import pickle
 import matplotlib.pyplot as plt
 from tensorflow.keras.preprocessing.image import img_to_array
 
 print(tf.keras.__version__)
-
-
-# # Get Images from folder and convert to GRAYSCALE, 128X128
-# files = glob.glob(PATH)
-# for f1 in files:
-# 	img = cv2.resize(cv2.imread(f1, cv2.IMREAD_GRAYSCALE), (IMG_SIZE, IMG_SIZE))
-# 	testing_data.append(img)
-#
-# testing_data = np.array(testing_data)
-# testing_data = testing_data.reshape((-1, IMG_SIZE, IMG_SIZE, 1))
-#
-# # Check to see if data went through
-# print('Testing Data Shape: ', np.shape(testing_data))
-# print(testing_data)
-
-# model = tf.keras.models.load_model('cnn_full_model_facemask')
-#
-# predictions = model.predict(testing_data[:])
-# answers = (np.argmax(predictions, axis=1))
-# for i in answers:
-# 	if i == 0:
-# 		print('Person is wearing the mask in a wrong way')
-# 	if i == 1:
-# 		print('Person is wearing a mask')
-# 	if i == 2:
-# 		print('Person is not wearing a mask')
-#
-# from matplotlib import pyplot
-# import matplotlib.pyplot as plt
-#
-# fig = plt.figure()
-#
-# labels = {0: 'Half Mask', 1: 'Mask', 2: 'No Mask'}
-#
-# for i in range(len(testing_data)):
-# 	subplot = fig.add_subplot(3, 5, i + 1)  #row, col, index
-# 	subplot.set_xticks([])
-# 	subplot.set_yticks([])
-# 	subplot.set_title(f'{labels[answers[i]]}')
-# 	subplot.imshow((testing_data[i]), cmap='gray')
-#
-# plt.show()
-
-
-
-
-
-# def draw_border(img, pt1, pt2, color, thickness, r, d):
-# 	x1,y1 = pt1
-# 	x2,y2 = pt2
-#
-# 	# Top left
-# 	cv2.line(img, (x1 + r, y1), (x1 + r + d, y1), color, thickness)
-# 	cv2.line(img, (x1, y1 + r), (x1, y1 + r + d), color, thickness)
-# 	cv2.ellipse(img, (x1 + r, y1 + r), (r, r), 180, 0, 90, color, thickness)
-#
-# 	# Top right
-# 	cv2.line(img, (x2 - r, y1), (x2 - r - d, y1), color, thickness)
-# 	cv2.line(img, (x2, y1 + r), (x2, y1 + r + d), color, thickness)
-# 	cv2.ellipse(img, (x2 - r, y1 + r), (r, r), 270, 0, 90, color, thickness)
-#
-# 	# Bottom left
-# 	cv2.line(img, (x1 + r, y2), (x1 + r + d, y2), color, thickness)
-# 	cv2.line(img, (x1, y2 - r), (x1, y2 - r - d), color, thickness)
-# 	cv2.ellipse(img, (x1 + r, y2 - r), (r, r), 90, 0, 90, color, thickness)
-#
-# 	# Bottom right
-# 	cv2.line(img, (x2 - r, y2), (x2 - r - d, y2), color, thickness)
-# 	cv2.line(img, (x2, y2 - r), (x2, y2 - r - d), color, thickness)
-# 	cv2.ellipse(img, (x2 - r, y2 - r), (r, r), 0, 0, 90, color, thickness)
-
-# ============================================================================
-
-# img = np.zeros((256,256,3), dtype=np.uint8)
 
 model = tf.keras.models.load_model('model.savedmodel', compile=False)
 
@@ -153,16 +72,3 @@ while True:
 
 	if cv.waitKey(1) == ord('q'):
 		break
-
-
-
-
-
-
-# CHECK IF IMAGES EXIST
-# use_samples = list(range(0,14))
-# for sample use_samples:
-#   # Generate a plot
-#   reshaped_image = testing_data[sample].reshape((IMG_SIZE, IMG_SIZE))
-#   plt.imshow(reshaped_image, cmap='gray')
-#   plt.show()
